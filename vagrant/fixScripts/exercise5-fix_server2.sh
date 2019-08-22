f278738395138571fa83a2b3b48f653d451f5d07
Adarbe <adarza@gmail.com>
2019-08-22 15:46:31 +0300

    .

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sshpass -p "vagrant" ssh-copy-id -i /etc/ssh/ssh_host_rsa_key.pub vagrant@server1
+sudo sed -i 's/^#   StrictHostKeyChecking ask/StrictHostKeyChecking no/' /etc/ssh/ssh_config
